0e714fcc85e61d58961d6ada01b1d7b0c0e056b9
Tatsiana Rudava <tatarudova.06@gmail.com>
2023-01-27 21:40:29 +0100

    var1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './sass/main.scss';
-import Swiper, { Pagination } from 'swiper';
+import Swiper, { Pagination, EffectFade } from 'swiper';
+var swiper = new Swiper( container, ".reviews-slider",  options, {
+    modules: [Pagination, EffectFade],
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    }
+)
